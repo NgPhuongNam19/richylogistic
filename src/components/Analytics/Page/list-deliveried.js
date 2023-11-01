352631e6e3aad0e65d90f639ae4537f637e7b6b6
PhuongNam <phuongnamforworks@gmail.com>
2023-11-01 15:43:29 +0700

    update

--- src/components/Analytics/Page/list-deliveried.js
+++ src/components/Analytics/Page/list-deliveried.js
@@ -4,7 +4,7 @@ import ItemDelivery from "../Views/box-item-deliver";
 export default function ListDeliveriedScreen( {route} ) {
     const {listDeliveried} = route.params;
 
-    return <FlatList style={{width: '99%', alignSelf: 'center', paddingTop: 10, paddingStart: 10, paddingEnd: 10}} data={listDeliveried} renderItem={({ item }) =>
+    return <FlatList style={{width: '100%', alignSelf: 'center', paddingTop: 10, paddingStart: 10, paddingEnd: 10}} data={listDeliveried} renderItem={({ item }) =>
         <ItemDelivery deliveryNumber={item.DeliveryNumber} marginBottom={15}/>
     } />
 }
